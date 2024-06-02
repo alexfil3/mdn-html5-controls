1441fa340d4afa90b9d4c4f62b758b620e9b3771
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-06-02 23:36:06 +0300

    completes html5 newer input types

---
+++ index.js
@@ -0,0 +1,8 @@
+const maxInvite = document.querySelector("#max-invite");
+const output = document.querySelector(".invite-output");
+
+output.textContent = maxInvite.value;
+
+maxInvite.addEventListener("input", () => {
+  output.textContent = maxInvite.value;
+});
